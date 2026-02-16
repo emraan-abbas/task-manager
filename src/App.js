705674b2dbdfdb6378813b452777ba2bfd2c5260
Imran Abbas <imranabbas67@gmail.com>
2026-02-16 13:59:16 -0700

    adding tasks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 import TaskForm from './components/TaskForm';
 
 function App() {
 
+  const [task, setTask] = useState([])
+
   const addTask = (task) => {
-    console.log('Task Data = ', task)
+    setTask(prev => [...prev, task]);
   }
 
   return (
-    <TaskForm addTask={addTask} />
+    <div>
+      <TaskForm addTask={addTask} />
+
+      <div className='container'>
+        <h3>Tasks:</h3>
+        {task.map(task => (
+          <div key={task.id} className='card p-2 mb-2'>
+            <h5>{task.title}</h5>
+            <h5>{task.description}</h5>
+            <h5>Priority: {task.priority}</h5>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
